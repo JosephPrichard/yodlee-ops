1aec41227da3dbdf52f69d07050f471867dd2841
JosephPrichard <58538077+JosephPrichard@users.noreply.github.com>
2026-03-07 12:38:35 -0600

    Adding terraform configuration for deployment

--- infra/main.tf
+++ infra/main.tf
@@ -30,14 +30,6 @@ resource "aws_s3_bucket_public_access_block" "main" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_public_access_block" "main" {
-  bucket                  = aws_s3_bucket.main.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_versioning" "main" {
   bucket = aws_s3_bucket.main.id
   versioning_configuration {
