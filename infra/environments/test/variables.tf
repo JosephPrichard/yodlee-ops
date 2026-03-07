5033fd385cf7d14fc3ae5553e13dd44701e51dd7
JosephPrichard <58538077+JosephPrichard@users.noreply.github.com>
2026-03-07 15:14:10 -0600

    Adding terraform configuration for deployment

--- infra/environments/test/variables.tf
+++ infra/environments/test/variables.tf
@@ -1,5 +1,5 @@
 variable "commit_sha" {
   description = "Commit SHA for this specific deployment"
   type        = string
-  default     = "" # overriden by github actions deployment
+  default     = "ddee415dcf05f9e6d8fb1287c48be0f98831c86a" # overriden by github actions deployment
 }
