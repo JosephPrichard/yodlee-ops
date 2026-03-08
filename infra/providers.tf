292aeb08da4bd12d07468ccc17c4316383de5e90
JosephPrichard <58538077+JosephPrichard@users.noreply.github.com>
2026-03-07 22:39:48 -0600

    Adding MSK to terraform deployment config

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 # you must comment this out the very first time `tfe deploy` is executed, it needs the msk cluster to be created on a previous deployment
 provider "kafka" {
-  bootstrap_servers = [aws_msk_cluster.main.bootstrap_brokers_sasl_iam]
+  bootstrap_servers = split(",", aws_msk_cluster.main.bootstrap_brokers_sasl_iam)
   tls_enabled       = true
   sasl_mechanism    = "aws-iam"
 }
